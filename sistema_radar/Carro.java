afc6b4cdc72e1c4a4031dee7aef4d1aecd359247
angell1506 <vitoramaralmarques@gmail.com>
2025-09-08 09:46:18 -0300

    add construtor

--- sistema_radar/Carro.java
+++ sistema_radar/Carro.java
@@ -2,10 +2,25 @@ package sistema_radar;
 
 public class Carro {
     public String placa;
-    public Integer velocidade;
+    private Integer velocidade;
     public Integer ano;
     public String modelo;
 
+    public Carro(String placa, Integer velocidade, Integer ano, String modelo){
+        this.placa = placa;
+        this.velocidade = velocidade;
+        this.ano = ano;
+        this.modelo = modelo;
+    }
+
+    public Integer getVelocidade(){
+        return this.velocidade;
+    }
+    public void setVelocidade(Integer velocidade){
+        if(velocidade >= 0){
+            this.velocidade = velocidade;
+        }
+    }
     public void acelerar() {
         this.velocidade += 10;
     }
--- sistema_radar/radar.java
+++ sistema_radar/radar.java
@@ -5,6 +5,11 @@ public class Radar {
     public String localizacao;
     public Integer limiteVelocidade;
 
+    public Radar(String localizacao, Integer limiteVelocidade){
+        this.localizacao = localizacao;
+        this.limiteVelocidade = limiteVelocidade;
+    }
+    
     public void emitirNotificacao(String placa, Integer velocidadeObservada){
         System.out.println("Placa: "+placa);
         System.out.println("Velocidade observada: "+velocidadeObservada);
@@ -13,8 +18,8 @@ public class Radar {
     }
 
     public void avaliarVelocidade(Carro carro){
-        if(carro.velocidade > this.limiteVelocidade){
-            emitirNotificacao(carro.placa, carro.velocidade);
+        if(carro.getVelocidade() > this.limiteVelocidade){
+            emitirNotificacao(carro.placa, carro.getVelocidade());
         }
     }
 }
