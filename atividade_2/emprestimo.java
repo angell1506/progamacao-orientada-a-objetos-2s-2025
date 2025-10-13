79f5b6e2a4ebb923d1528ce9ab19e38b816a2b07
angell1506 <vitoramaralmarques@gmail.com>
2025-10-13 09:10:24 -0300

    add mudanças

--- atividade_2/emprestimo.java
+++ atividade_2/emprestimo.java
@@ -13,6 +13,40 @@ public class emprestimo {
         this.dataDevolucao = dataDevolucao;
     }
 
+
+    public pessoa getPessoa() {
+        return this.pessoa;
+    }
+
+    public void setPessoa(pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public material getMaterial() {
+        return this.material;
+    }
+
+    public void setMaterial(material material) {
+        this.material = material;
+    }
+
+    public String getDataEmprestimo() {
+        return this.dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(String dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public String getDataDevolucao() {
+        return this.dataDevolucao;
+    }
+
+    public void setDataDevolucao(String dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+
     public void exibirDetalhes() {
         System.out.println("Data do Empréstimo: " + dataEmprestimo);
         System.out.println("Data de Devolução: " + dataDevolucao);
